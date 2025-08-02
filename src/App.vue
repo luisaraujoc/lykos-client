acfe7fecb380ebe4903bb419ad3d9caa8cbd7dfe
luisaraujoc <luislk_@outlook.com>
2025-08-02 18:20:22 -0300

    Add Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonSolid from './components/ButtonSolid.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flew flex-row items-center justify-center">
+    <h1 class="text-3xl font-bold">Welcome to Lykos</h1>
+    <p class="text-lg mt-2">This is a simple Vue.js application.</p>
+    <p class="text-sm mt-1">Feel free to explore the components.</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ButtonSolid text="Click me" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/ButtonSolid.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  text: string;
+  icon?: string; // Optional icon prop
+}>();
+
+</script>
+
+<template>
+    <!-- Button solid with text sended of props -->
+    <button class="btn-primary-solid">
+        <span>{{ props.text }}</span>
+        <!-- <span v-if="props.icon" class="icon">
+            <i :class="props.icon"></i>
+        </span> -->
+    </button>
+</template>
