bc007a222b0d78f64fe23683cc9020188d1c7dd9
FabricioS0 <fsoliveira899@gmail.com>
2025-08-11 14:24:06 -0300

    Atualização no app.vue, inicio do cadastro Freelancer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import CadastroUsuario from '/@/components/CadastroUsuario.vue';
+import CadastroUsuario from './components/CadastroUsuario.vue';
+import CadastroFreelancer from './components/CadastroFreelancer.vue';
+
 </script>
 
 <template>
- <CadastroUsuario />
+ <CadastroFreelancer />
 </template>
---
+++ src/components/CadastroFreelancer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import navBarCadastros from './navBarCadastros.vue';
+</script>
+
+<template>
+<navBarCadastros />
+    <body>
+        <div class="flex items-center justify-center min-h-screen flex-col max-w-800 mx-auto p-4">
+            <h1 class="text-2xl font-bold mb-6 text-center">
+                torne-se um freelancer
+            </h1>
+                
+        </div>
+    </body>
+</template>
+
+
